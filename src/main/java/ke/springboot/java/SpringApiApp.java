b8e38e723ecb86ae3484783a510668fd6ab465cc
Dennis Peters <dennis95peters@gmail.com>
2021-11-03 10:04:24 +0300

    initialise and configure spring boot by running a simple resource URI

---
+++ src/main/java/ke/springboot/java/SpringApiApp.java
@@ -0,0 +1,16 @@
+package ke.springboot.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SpringApplication.run(SpringApiApp.class, args);
+
+	}
+
+}
